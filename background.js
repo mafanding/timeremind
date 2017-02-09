df298d62e7d0cf67bd2fef8d3fceeab738a1b4b7
mfdgood <mfdboy@163.com>
2017-02-09 12:45:44 +0800

    add ignore hour option

--- background.js
+++ background.js
@@ -5,15 +5,16 @@ setInterval(function (){
         var minute=date.getMinutes();
         var week=date.getDay();
         var weekMap=["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
-        if (minute==localStorage['minute'] && hour>=localStorage['start_time']&& hour<=localStorage['end_time'] &&(hour!=11|| hour!=12|| hour!=13)&& 1==localStorage[weekMap[week]]) {
-            var n = new Notification('提醒', {
-                body : '时间到了',
-                icon : './favicon.ico'
-            });
-            
-            n.onclick=function (){
-                n.close();
-                console.log(n);
+        if (minute==localStorage['minute'] && hour>=localStorage['start_time']&& hour<=localStorage['end_time'] && 1==localStorage[weekMap[week]]) {
+            var prefix='ignore_';
+            if(localStorage[prefix+hour]!=1){
+                var n = new Notification('Notification!', {
+                    body : 'It\'s time for having a rest!',
+                    icon : './favicon.ico'
+                });
+                n.onclick=function (){
+                    n.close();
+                }
             }
         }
     }
--- options.js
+++ options.js
@@ -1,23 +1,60 @@
-var startTime=document.getElementById('start_time');
-var endTime=document.getElementById('end_time');
-var minute=document.getElementById('minute');
-var save=document.getElementById('save');
+var $=document;
+var startTime=$.getElementById('start_time');
+var endTime=$.getElementById('end_time');
+var minute=$.getElementById('minute');
+var save=$.getElementById('save');
+var ignoreHour=$.getElementById('ignore_hour');
 var ToolObject=new Object();
-ToolObject.createOptionNode=function (a,b){
-	var option=document.createElement('option');
+ToolObject.createOptionNode=function (b){
+	var option=$.createElement('option');
 	option.value=b;
 	option.textContent=b;
-	a.appendChild(option);
+	this.previousNode=this.currentNode;
+	this.currentNode=option;
+	return this;
+}
+ToolObject.createCheckboxNode=function (b,c){
+	var checkbox=$.createElement('input');
+	checkbox.type='checkbox';
+	checkbox.value=b;
+	checkbox.id=c;
+	this.previousNode=this.currentNode;
+	this.currentNode=checkbox;
+	return this;
+}
+ToolObject.createTdNode=function (b){
+	var td=$.createElement('td');
+	td.textContent=b;
+	this.previousNode=this.currentNode;
+	this.currentNode=td;
+	return this;
+}
+ToolObject.appendTo=function (a){
+	a.appendChild(this.currentNode);
+	return this;
+}
+ToolObject.appendChild=function (a){
+	this.currentNode.appendChild(a);
+	return this;
+}
+ToolObject.clearChildren=function (a){
+	while (a.lastChild) {
+	  a.removeChild(a.lastChild);
+	}
+}
+ToolObject.resetCurrentNode=function (){
+	this.currentNode=this.previousNode;
+	return this;
 }
 for (var i=0;i<24;i++){
-	ToolObject.createOptionNode(startTime,i);
-	ToolObject.createOptionNode(endTime,i);
+	ToolObject.createOptionNode(i).appendTo(startTime);
+	ToolObject.createOptionNode(i).appendTo(endTime);
 }
 for (var i=0;i<60;i++){
-	ToolObject.createOptionNode(minute,i);
+	ToolObject.createOptionNode(i).appendTo(minute);
 }
 save.addEventListener('click',function(e){
-	var inputs=document.getElementsByTagName('input');
+	var inputs=$.getElementsByTagName('input');
 	for(var i=0;i<inputs.length;i++){
 		if (inputs.item(i).checked) {
 			localStorage[inputs.item(i).id]=1;
@@ -25,23 +62,34 @@ save.addEventListener('click',function(e){
 			localStorage[inputs.item(i).id]=0;
 		}
 	}
-	var selects=document.getElementsByTagName('select');
+	var selects=$.getElementsByTagName('select');
 	for(var i=0;i<selects.length;i++){
 		localStorage[selects.item(i).id]=selects.item(i).value;
 	}
 });
 function loadLocalStorageValue(){
-	var inputs=document.getElementsByTagName('input');
-	var selects=document.getElementsByTagName('select');
-	for(var i=0;i<inputs.length;i++){
-		if(localStorage[inputs.item(i).id]==1){
-			inputs.item(i).checked=true;
-		}
-	}
+	var selects=$.getElementsByTagName('select');
 	for(var i=0;i<selects.length;i++){
 		if(localStorage[selects.item(i).id]){
 			selects.item(i).selectedIndex=localStorage[selects.item(i).id];
 		}
 	}
+	updateIgnoreHourValue();
+	var inputs=$.getElementsByTagName('input');
+	for(var i=0;i<inputs.length;i++){
+		if(localStorage[inputs.item(i).id]==1){
+			inputs.item(i).checked=true;
+		}
+	}
 }
 window.addEventListener('load',loadLocalStorageValue);
+function updateIgnoreHourValue(){
+	var prefix='ignore_';
+	ToolObject.clearChildren(ignoreHour);
+	ignoreHour.appendChild(ToolObject.createTdNode('ignore hour：').currentNode);
+	for(var i=parseInt(startTime.value);i<=parseInt(endTime.value);i++){
+		ToolObject.createTdNode(i).createCheckboxNode(i,prefix+i).appendTo(ToolObject.previousNode).resetCurrentNode().appendTo(ignoreHour);
+	}
+}
+startTime.addEventListener('change',updateIgnoreHourValue);
+endTime.addEventListener('change',updateIgnoreHourValue);
